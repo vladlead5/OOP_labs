08baff3342b1ca2faec9cea5c444873fa879833b
Vladislav Reznik <137673057+vladlead5@users.noreply.github.com>
2024-09-25 21:23:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "isCleanNumber.h"
 
 int main() {
-    std::cout << "Hello World!";
+    int number;
+    std::cout << "Введите число: ";
+    std::cin >> number;
+    if (isCleanNumber(number)) {
+        std::cout << "Число чистое" << std::endl;
+    } else {
+        std::cout << "Число не является чистым" << std::endl;
+    }
     return 0;
 }
